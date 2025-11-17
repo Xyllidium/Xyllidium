9a6526fffa38fc95f2b2b040f837a55d7708d9ad
Raunak Singh <raunaksingh_27@hotmail.com>
2025-11-17 22:35:41 +0530

    Delete wallet/xcc directory

--- wallet/xcc/src/xhss/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod xmss;
-pub mod sphincs;
-pub mod hybrid;
-
-pub use xmss::{XmssKeypair, XmssSecretKey, XmssPublicKey, XmssSignature};
-pub use sphincs::{SphincsSecretKey, SphincsPublicKey, SphincsSignature};
-pub use hybrid::{XhssKeypair, XhssSignature};
